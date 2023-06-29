0c932ee00d20c1b9937163902368f0213154579f
Joan254 <joanotieno1998@gmail.com>
2023-06-29 20:04:09 +0000

    Function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,30 +13,18 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a, b, c;
-
-	int sum = 0, carry = 0;
-
-	int length1 = 0;
-
-	int length2 = 0;
+	int a, b, c, sum = 0, carry = 0, length1 = 0, length2 = 0;
 
 	char temp;
 
 	while (n1[length1] != '\0')
-	{
 		length1++;
-	}
 
 	while (n2[length2] != '\0')
-	{
 		length2++;
-	}
 
 	if (length1 >= size_r || length2 >= size_r)
-	{
 		return (0);
-	}
 
 	a = length1 - 1;
 	b = length2 - 1;
@@ -54,13 +42,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		carry = sum / 10;
 		r[c++] = sum % 10 + '0';
 	}
-	
+
 	for (a = 0, b = c - 1; a < b; a++, b--)
 	{
 		temp = r[a];
-
 		r[a] = r[b];
-
 		r[b] = temp;
 
 	}
