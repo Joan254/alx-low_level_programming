7e4797e355cd963c7b8929061922ece74e554180
Joan254 <joanotieno1998@gmail.com>
2023-06-30 08:25:01 +0000

    Function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,33 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+
+	int length = 0;
+
+	int a;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (a = 0; a < length; a++)
+	{
+		length--;
+		rev = s[a];
+		s[a] = s[length];
+		s[length] = rev;
+	}
+}
 
 /**
  * infinite_add - adds two numbers
@@ -13,44 +41,43 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a, b, c, sum = 0, carry = 0, length1 = 0, length2 = 0;
+	int carry = 0, a = 0, b = 0, digits = 0;
 
-	char temp;
+	int v1 = 0, v2 = 0, sum = 0;
 
-	while (n1[length1] != '\0')
-		length1++;
-
-	while (n2[length2] != '\0')
-		length2++;
-
-	if (length1 >= size_r || length2 >= size_r)
+	while (*(n1 + a) != '\0')
+		a++;
+	while (*(n2 + b) != '\0')
+		b++;
+	a--;
+	b--;
+	if (a >= size_r || b >= size_r)
 		return (0);
-
-	a = length1 - 1;
-	b = length2 - 1;
-	c = 0;
-
-	while (a >= 0 || b >= 0 || carry)
+	while (a >= 0 || b >= 0 || carry == 1)
 	{
-		sum = carry;
-
-		if (a >= 0)
-			sum += n1[a--] - '0';
-		if (b >= 0)
-			sum += n2[b--] - '0';
-
-		carry = sum / 10;
-		r[c++] = sum % 10 + '0';
+		if (a < 0)
+			v1 = 0;
+		else
+			v1 = *(n1 + a) - '0';
+		if (b < 0)
+			v2 = 0;
+		else
+			v2 = *(n2 + b) - '0';
+		sum = v1 + v2 + carry;
+		if (sum >= 10)
+			carry = 1;
+		else
+			carry = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (sum % 10) + '0';
+		digits++;
+		b--;
+		a--;
 	}
-
-	for (a = 0, b = c - 1; a < b; a++, b--)
-	{
-		temp = r[a];
-		r[a] = r[b];
-		r[b] = temp;
-
-	}
-	r[c] = '\0';
-
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
