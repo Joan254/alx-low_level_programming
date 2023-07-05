8e43e5665061faac9303cd148f4a0ff0bd67767f
Joan254 <joanotieno1998@gmail.com>
2023-07-05 14:52:07 +0000

    Function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if the strings can be considered identical,
+ * otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,3 +10,4 @@ int is_prime_number(int n);
 int _prime(int n, int a);
 int _palindrome(char *s, int p);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
