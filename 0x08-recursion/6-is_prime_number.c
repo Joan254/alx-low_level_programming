53c037194985d1e3081b6a253ee30281a012921d
Joan254 <joanotieno1998@gmail.com>
2023-07-05 13:21:09 +0000

    Function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _prime - finds if a number is prime
+ * @n: the number
+ * @a: iterator
+ *
+ * Return: 1 if n is a prime number 0 otherwise
+ */
+
+int _prime(int n, int a)
+{
+	if (a == 1)
+	{
+		return (1);
+	}
+
+	if (n % a == 0 && a > 0)
+	{
+		return (0);
+	}
+
+	return (_prime(n, a - 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ * @n: the number
+ *
+ * Return: 1 if n is a prime number 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+
+	return (_prime(n, n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int a);
+int is_prime_number(int n);
+int _prime(int n, int a);
