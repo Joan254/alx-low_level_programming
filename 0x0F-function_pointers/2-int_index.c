cbf29ccac6ad45f2fe5aae63d11df12e75e703c7
Joan254 <joanotieno1998@gmail.com>
2023-07-19 10:45:52 +0000

    Function that searches for an ineger

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,6 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
