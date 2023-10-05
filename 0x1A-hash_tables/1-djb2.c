965102541148d6980e4536131739ab56cd21e719
Joan254 <joanotieno1998@gmail.com>
2023-10-05 12:10:56 +0000

    Hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,9 +9,10 @@
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash;
 	int count;
 
+	hash = 5381;
 	while ((count = *str++))
 		hash = ((hash << 5) + hash) + count; /*hash * 33 + count*/
 	return (hash);
